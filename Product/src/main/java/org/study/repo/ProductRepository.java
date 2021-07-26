aa10f78546c64bac417821aacb5ea053d365d036
giitjavateam <87560951+giitjavateam@users.noreply.github.com>
2021-07-26 13:27:51 +0530

    Delete Product directory

--- Product/src/main/java/org/study/repo/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package org.study.repo;
-import org.springframework.data.repository.CrudRepository;
-import org.study.model.Product;
-public interface ProductRepository extends CrudRepository<Product,Integer>{// crud- create read update and delete
-
-}
